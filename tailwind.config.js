e6953bf8eaffa1ed688cd1b54414622113ded37e
Sajid Sheikh <sajidsheikh1998@gmail.com>
2021-12-11 13:38:15 +0530

    task/ added next-themes and dark mode support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   mode: 'jit',
-  purge: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-  ],
-  darkMode: false, // or 'media' or 'class'
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
